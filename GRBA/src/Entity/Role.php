58617d9e29c530cb083400616bc55f9eacb53eb4
Franck-Valet <franck.valet.pro@gmail.com>
2019-11-07 13:26:05 +0100

    name's Role convert to string

--- GRBA/src/Entity/Role.php
+++ GRBA/src/Entity/Role.php
@@ -31,6 +31,12 @@ class Role
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="role")
      */
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+    
     private $users;
 
     public function __construct()
@@ -97,4 +103,6 @@ class Role
 
         return $this;
     }
+
+
 }
