8e22156fe4b2539d27c8fddaa36e090c028c02bd
Franck-Valet <franck.valet.pro@gmail.com>
2019-11-09 13:58:43 +0100

    footer OK

--- GRBA/src/Controller/MainController.php
+++ GRBA/src/Controller/MainController.php
@@ -6,17 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Event;
 use App\Repository\EventRepository;
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
      */
-    public function index(EventRepository $eventRepository)
+    public function index(EventRepository $eventRepository, ContactRepository $contactRepository)
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             'events' => $eventRepository->findAll(),
+            'contacts' => $contactRepository->findAll(),
         ]);
     }
 }
