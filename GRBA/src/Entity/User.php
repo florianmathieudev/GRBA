711da6da790dc811a2c758a4d4004bcc46e4d460
Franck-Valet <franck.valet.pro@gmail.com>
2019-11-12 12:30:39 +0100

    login & registration OK

--- GRBA/src/Entity/User.php
+++ GRBA/src/Entity/User.php
@@ -3,11 +3,20 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraint\Email;
+// use Symfony\Component\Validator\Constraints\Lenght;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *  fields={"email"},
+ *  message="L'email que vous avez indiqué est déjà utilisé !"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -18,19 +27,36 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     * @Assert\Length(max="55", maxMessage="Vous ne pouvez pas utiliser un email qui fait plus de 55 caractères !")
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Votre mot de passe doit faire minimum 8 caractères !")
+     * @Assert\Length(max="16", maxMessage="Votre mot de passe doit faire maximum 16 caractères !")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="Vous n'avez pas écrit le même mot de passe")
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Vous n'avez pas écrit le même mot de passe")
+     */
+    private $confirm_password;
+    
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
      * @ORM\JoinColumn(nullable=false)
      */
     private $role;
+
+    /**
+     * @Assert\Length(min="8", minMessage="Votre nom d\'utilisateur doit faire minimum 8 caractères !")
+     * @Assert\Length(max="16", maxMessage="Votre nom d\'utilisateur doit faire maximum 16 caractères !")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
 
     public function getId(): ?int
     {
@@ -72,5 +98,37 @@ class User
 
         return $this;
     }
+    
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+    
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        
+        return $this;
+    }
+    
+    public function getConfirmPassword()
+    {
+        return $this->confirm_password;
+    }
+
+    public function setConfirmPassword($confirm_password)
+    {
+        $this->confirm_password = $confirm_password;
+
+        return $this;
+    }
+
+    public function getRoles() {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt() {}
+
+    public function eraseCredentials() {}
 
 }
