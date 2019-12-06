6002da31e64ee3e160d19130b1cba9b79650b549
KnHFr <franck.valet.pro@gmail.com>
2019-12-06 10:33:05 +0100

    event index rework for show and create type and event

--- GRBA/src/Controller/Backend/BackController.php
+++ GRBA/src/Controller/Backend/BackController.php
@@ -4,23 +4,34 @@ namespace App\Controller\Backend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Contact;
 use App\Repository\ContactRepository;
-use App\Entity\Header;
-use App\Repository\HeaderRepository;
-use App\Entity\Footer;
-use App\Repository\FooterRepository;
+use App\Repository\TypeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Event;
+use App\Form\EventType;
 
 class BackController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin", methods={"GET","POST"})
      */
-    public function index(ContactRepository $contactRepository, HeaderRepository $headerRepository, FooterRepository $footerRepository)
+    public function index(Request $request, ContactRepository $contactRepository, TypeRepository $typeRepository): Response
     {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin');
+        }
         return $this->render('back/index.html.twig', [
             'controller_name' => 'BackController',
-            'contacts' => $contactRepository->findAll()
+            'types' => $typeRepository->findAll(),
+            'event' => $event,
+            'form' => $form->createView(),
         ]);
     }
 }
