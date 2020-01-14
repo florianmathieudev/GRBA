c26d724bc95a5dd9ed40f3741af07037224b2872
KnHFr <franck.valet.pro@gmail.com>
2020-01-14 10:01:07 +0100

    link for download document fixed in /telechargement

--- GRBA/src/Controller/SecurityController.php
+++ GRBA/src/Controller/SecurityController.php
@@ -4,17 +4,19 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+// use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $form =$this->createForm(RegistrationType::class, $user);
