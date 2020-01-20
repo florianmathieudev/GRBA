b24731db233dd355e3c654729ce8bce3439a8976
florianmathieudev <florianmathieu.dev@gmail.com>
2020-01-20 22:30:42 +0100

    layout next events

---
+++ GRBA/public/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('.your-class').slick({
+	  dots: true,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+	  {
+		breakpoint: 1424,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: true
+		}
+	  },
+	  {
+		breakpoint: 1100,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 800,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+	});
+  });  
